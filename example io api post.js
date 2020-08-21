0419f54452e463dcaf05b600e7c7d227adb4334c
Huy Nguyen <huyndx@outlook.com>
2020-08-21 17:20:19 +0700

    Update

--- example io api post.js
+++ example io api post.js
@@ -1,2 +1,2 @@
 web.post("https://iot.ioeasy.com/api/23eb074da31972a4?apiKey=ciR5N0HBOrbm0ffZc42JBLI3hQtVtXvl",{"cmd": "#935"})
-.then(data => {out = JSON.stringify(data.toString())})
+.then(data => {out = JSON.stringify(data)})
