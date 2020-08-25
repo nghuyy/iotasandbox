7226b5efdb3a1db5cf34a4358209993a2089ca45
Huy Nguyen <huyndx@outlook.com>
2020-08-26 01:07:31 +0700

    Update

--- iota.js
+++ iota.js
@@ -1,6 +1,8 @@
 IOTA  = {
     //Tên người dùng
     user: "",
+    // Trạng thái kích hoạt sdt IOTA
+    phone_act:true,
     //Dữ liệu truyền lên từ địa chỉ trình duyệt hoặc người dùng nhập vào
     mess: {}
 };
---
+++ iota_activate_check.js
@@ -0,0 +1,6 @@
+// Kiểm tra trạng thái tài khoản
+if(IOTA.phone_act){
+    out=`Tài khoản ${IOTA.user} đã kích hoạt`;
+}else{
+    out=`Tài khoản ${IOTA.user} vừa tạo và chưa kích hoạt`;
+}
