866423c90da16a81d79bf232b3db94f3ce55fea8
Huy Nguyen <huyndx@outlook.com>
2020-11-07 10:13:48 +0700

    Update new sandbox with TelegramSend feature

--- libs/IOTA.js
+++ libs/IOTA.js
@@ -14,4 +14,27 @@ IOTA  = {
  *  Gởi một đoạn tin nhắn đến kênh Telegram đã link với hệ thống
  * @param msg Nội dung
  */
-postTChanel = (msg)=>{};
+
+TelegramSend = (msg)=>{};
+
+
+uniqid = length =>{
+    var result = '';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+dePrinterCode = (code)=>{
+    stringin = stringin.replace(/\[s3\]/g, '');
+    stringin = stringin.replace(/\[s2\]/g, '');
+    stringin = stringin.replace(/\[s1\]/g, '');
+    stringin = stringin.replace(/\[s0\]/g, '');
+    stringin = stringin.replace(/\[ac\]/g, '');
+    stringin = stringin.replace(/\[al\]/g, '');
+    stringin = stringin.replace(/\[ar\]/g, '');
+    return stringin;
+};
---
+++ libs/uuid.js
@@ -0,0 +1,1 @@
+uuid = require("uuid");
